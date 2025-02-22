8143b6402b54cd1b032bcb94c4641f92f3be05cc
armirigj <armirgjini76@gmail.com>
2025-02-22 22:38:07 +0100

    Adding exercise 8 to exercise 7

--- src/main/java/ex7/Exercise7.java
+++ src/main/java/ex7/Exercise7.java
@@ -1,9 +1,11 @@
 package ex7;
 
 import java.util.Arrays;
+import java.util.Comparator;
 import java.util.List;
 import java.util.NoSuchElementException;
 import java.util.Objects;
+import java.util.stream.Collectors;
 
 public class Exercise7 {
     public static void main (String[] args){
@@ -12,6 +14,11 @@ public class Exercise7 {
                 new Employee("K092345729Z", "John Doe", 45000),
                 new Employee("H623454687A", "Jessie Doe", 30500)
         );
+
+        List<Employee> sortByName = employees.stream()
+                .sorted(Comparator.comparing(Employee :: getNames))
+                .collect(Collectors.toList());
+
         Employee salary = employees.stream()
                 .filter(s -> s.getSalary() >= 30000)
                 .filter(Objects::nonNull)
@@ -19,6 +26,8 @@ public class Exercise7 {
                 .orElseThrow(() -> new NoSuchElementException("There is no employee found with salary lower than 30000"));
 
         System.out.println("First eligible employee found: " + salary);
+        System.out.println("Employees names sorted: " + sortByName);
 
     }
+
 }
